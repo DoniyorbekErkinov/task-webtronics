e731a19c5d2a9bf380495db514d710a412aee659
doniyor <erkinovdoniyor03@gmail.com>
2023-12-24 01:42:13 +0500

    .

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,5 @@
 import { route } from "quasar/wrappers";
-import {
-  createRouter,
-  createMemoryHistory,
-  createWebHistory,
-  createWebHashHistory,
-} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 
 /*
